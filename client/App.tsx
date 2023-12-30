490f4304238ea54435e31449bb96c77461de2e6f
Quise <darkquise@gmail.com>
2023-12-30 11:10:19 -0500

    new structure

--- client/App.tsx
+++ client/App.tsx
@@ -7,6 +7,7 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
+import Login from './src/Components/Screens/Login/Login';
 import {
   SafeAreaView,
   ScrollView,
@@ -63,36 +64,37 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Login />
+    // <SafeAreaView style={backgroundStyle}>
+    //   <StatusBar
+    //     barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+    //     backgroundColor={backgroundStyle.backgroundColor}
+    //   />
+    //   <ScrollView
+    //     contentInsetAdjustmentBehavior="automatic"
+    //     style={backgroundStyle}>
+    //     <Header />
+    //     <View
+    //       style={{
+    //         backgroundColor: isDarkMode ? Colors.black : Colors.white,
+    //       }}>
+    //       <Section title="Step One">
+    //         Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+    //         screen and then come back to see your edits.
+    //       </Section>
+    //       <Section title="See Your Changes">
+    //         <ReloadInstructions />
+    //       </Section>
+    //       <Section title="Debug">
+    //         <DebugInstructions />
+    //       </Section>
+    //       <Section title="Learn More">
+    //         Read the docs to discover what to do next:
+    //       </Section>
+    //       <LearnMoreLinks />
+    //     </View>
+    //   </ScrollView>
+    // </SafeAreaView>
   );
 }
 
---
+++ client/src/Components/Screens/Login/Login.tsx
@@ -0,0 +1,61 @@
+import {useState} from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native'
+
+
+const Login = () => {
+  const [ username, setUsername] = useState('')
+  const [ password, setPassword] = useState('')
+
+
+  return (
+    <View style={styles.layout}>
+      <View>
+        <Text style={styles.title}>Login</Text>
+        <View style={styles.loginContainer}>
+          <View style={styles.inputContainer}>
+            <Text>Username</Text>
+            <TextInput
+              style={styles.textInput}
+              maxLength={20}
+             />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text>Password</Text>
+            <TextInput style={styles.textInput} />
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    maxWidth: '100%'
+  },
+  loginContainer: {
+    position: 'relative',
+    top: '50%'
+  },
+
+  title: {
+    textAlign: 'center',
+    fontSize: 50,
+    marginTop: 30
+
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  textInput: {
+    borderWidth: 2,
+    borderRadius: 5,
+    maxWidth: '80%',
+    backgroundColor: "grey"
+  },
+});
+
+export default Login;
