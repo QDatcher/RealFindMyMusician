9537c9410158f971bb6e942851fae37dc8a8808c
Quise <darkquise@gmail.com>
2024-01-05 19:18:20 -0500

    Added to the Login Screen

--- client/src/Components/Screens/Login/Login.tsx
+++ client/src/Components/Screens/Login/Login.tsx
@@ -1,32 +1,96 @@
 import {useState} from 'react';
-import {View, Text, TextInput, StyleSheet} from 'react-native';
-import { Button } from "native-base";
+import {View, TextInput, StyleSheet} from 'react-native';
+import {Button, Input, Center, Box, Heading, VStack, FormControl, Link, HStack, Text} from 'native-base';
 
 const Login = () => {
-  const [ username, setUsername] = useState('')
-  const [ password, setPassword] = useState('')
+  const [ username, setUsername] = useState('');
+  const [ password, setPassword] = useState('');
 
+
+  const onSubmit = (e) => {
+
+  }
 
   return (
-    <View style={styles.layout}>
-      <View>
-        <Text style={styles.title}>Login</Text>
-        <View style={styles.loginContainer}>
-          <View style={styles.inputContainer}>
-            <Text>Username</Text>
-            <TextInput
-              style={styles.textInput}
-              maxLength={20}
-             />
-          </View>
-          <View style={styles.inputContainer}>
-            <Text>Password</Text>
-            <TextInput style={styles.textInput} />
-            <Button size="sm" variant="ghost">Tester Button</Button>
-          </View>
-        </View>
-      </View>
-    </View>
+    //I took this from Native Base and added some slight changes. Might change it up a bit more though
+
+    <Center w="100%">
+      <Box safeArea p="2" py="8" w="90%" maxW="290">
+        <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
+        color: "warmGray.50"
+      }}>
+          Musician Finder
+        </Heading>
+        <Heading mt="1" _dark={{
+        color: "warmGray.200"
+      }} color="coolGray.600" fontWeight="medium" size="xs">
+          Sign in to continue!
+        </Heading>
+
+        <VStack space={5} mt="5">
+          <FormControl>
+            <FormControl.Label>Email ID</FormControl.Label>
+            <Input 
+              placeholder='Email'              
+              variant="rounded"
+              onChange={e => setUsername(e.target.value)} />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Password</FormControl.Label>
+            <Input 
+              variant="rounded"
+              placeholder='Password' 
+              type="password"
+              onChange={e => setPassword(e.target.value)} />
+            <Link _text={{
+            fontSize: "xs",
+            fontWeight: "500",
+            color: "indigo.500"
+          }} alignSelf="flex-end" mt="1">
+              Forget Password?
+            </Link>
+          </FormControl>
+          <Button mt="2" colorScheme="indigo">
+            Sign in
+          </Button>
+          <HStack mt="6" justifyContent="center">
+            <Text fontSize="sm" color="coolGray.600" _dark={{
+            color: "warmGray.200"
+          }}>
+              I'm a new user.{" "}
+            </Text>
+            <Link _text={{
+            color: "indigo.500",
+            fontWeight: "medium",
+            fontSize: "sm"
+          }} href="#">
+              Sign Up
+            </Link>
+          </HStack>
+        </VStack>
+      </Box>
+    </Center>
+
+
+//Was originally trying this. Might go with the round ended inputs still but
+    // <View style={styles.layout}>
+    //     <Text style={styles.title}>Login</Text>
+    //     <View style={styles.loginContainer}>
+    //       <Input 
+    //           maxLength={20} 
+    //           variant="rounded"
+    //           placeholder="Username"
+    //           onChange={(e)=>setUsername(e.target.value)} />
+    //       <Input 
+    //           maxLength={20} 
+    //           variant="rounded"
+    //           placeholder="Username"
+    //           onChange={(e)=>setUsername(e.target.value)} />
+          
+    //         <Button size="sm" variant="ghost">Tester Button</Button>
+          
+    //     </View>
+    // </View>
   )
 }
 
@@ -35,21 +99,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     alignItems: 'center',
-    maxWidth: '100%'
+    width: '100%'
   },
   loginContainer: {
     position: 'relative',
-    top: '50%'
+    backgroundColor: 'grey',
+    width: '80%',
+    height: '80%'
+    
   },
 
   title: {
     textAlign: 'center',
     fontSize: 50,
-    marginTop: 30
+    marginTop: 70
 
   },
   inputContainer: {
-    width: "80%"
+    width: "80%",
+    flex: 1,
+
   },
   textInput: {
     borderWidth: 2,
